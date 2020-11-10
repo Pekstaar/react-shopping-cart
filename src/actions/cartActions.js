0319abb5c6de5e7016b0ec33f7222f9c5239fc7a
Pekmah <pekstaar@gmail.com>
2020-11-11 02:28:38 +0300

    redux to cart issue

--- src/actions/cartActions.js
+++ src/actions/cartActions.js
@@ -20,9 +20,9 @@ export const addToCart = (product) => (dispatch, getState) => {
 };
 
 export const removeFromCart = (product) => (dispatch, getState) => {
-  const cartItems = getState()
-    .cart.cartItems.slice()
-    .filter((x) => x._id !== product._id);
-  dispatch({ type: REMOVE_FROM_CART, payload: { cartItems } });
-  localStorage.setItem("cartItems", JSON.stringify(cartItems));
-};
+    const cartItems = getState()
+      .cart.cartItems.slice()
+      .filter((x) => x._id !== product._id);
+    dispatch({ type: REMOVE_FROM_CART, payload: { cartItems } });
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  };
